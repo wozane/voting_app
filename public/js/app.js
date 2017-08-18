1fdb4fec5b202799ed97fab4c204e86ebb59835b
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-08-18 14:19:42 +0200

    Refactor ProductList class

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,13 @@
 class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      products: [],
-    };
-
-    this.handleProductUpVote = this.handleProductUpVote.bind(this);
-  }
+  state = {
+    products: [],
+  };
 
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
-  handleProductUpVote(productId) {
+  handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
       if (product.id === productId) {
         return Object.assign({}, product, {
