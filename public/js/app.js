921dcc384c228f6ca018365852461d75baf25163
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-08-18 14:03:58 +0200

    Set 'state' for voting

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,35 @@
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
   handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
   }
 
   render() {
-    const products = Seed.products.sort((a,b) => (
+    const products = this.state.products.sort((a,b) => (
       b.votes - a.votes
     ));
     const productComponents = products.map((product) => (
